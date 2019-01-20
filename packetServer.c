3a0ec1c36b6beae7f4dae7143c8a63673efa299e
Zach Shaw <zachthm@gmail.com>
2019-01-19 18:22:38 -0700

    working condition of server and client

--- packetServer.c
+++ packetServer.c
@@ -17,34 +17,36 @@
 // Initialize the server, returns the socket file descriptor of the binded port.
 int init_server()
 {
-	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     printf("%d\n", sockfd);
-	struct sockaddr_in servaddr;
+    struct sockaddr_in servaddr;
 
-	// Creating server socket file descriptor
-	if (sockfd < 0) {
-		perror("socket creation failed");
-		exit(EXIT_FAILURE);
-	}
+    // Creating server socket file descriptor
+    if (sockfd < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-	memset(&servaddr, 0, sizeof(servaddr));
+    memset(&servaddr, 0, sizeof(servaddr));
 
-	// Filling server information
-	servaddr.sin_family = AF_INET; // IPv4
-	servaddr.sin_addr.s_addr = INADDR_ANY;
-	servaddr.sin_port = htons(PORT);
+    // Filling server information
+    servaddr.sin_family = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
 
-	int bind_ret = bind(sockfd, (const struct sockaddr *)&servaddr,
-			    sizeof(servaddr));
+    int bind_ret = bind(sockfd, (const struct sockaddr *)&servaddr,
+                        sizeof(servaddr));
 
-	// Bind the socket with the server address
-	if (bind_ret < 0) {
-		perror("bind failed");
-		close(sockfd);
-		exit(EXIT_FAILURE);
-	}
+    // Bind the socket with the server address
+    if (bind_ret < 0)
+    {
+        perror("bind failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
 
-	return sockfd;
+    return sockfd;
 }
 
 int main(int argc, char *argv[])
@@ -73,25 +75,23 @@ int main(int argc, char *argv[])
     printf("SIZE: %d\n", tempVal);
 
     // Creating socket file descriptor
-    
+
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    
     memset(&servaddr, 0, sizeof(servaddr));
     memset(&cliaddr, 0, sizeof(cliaddr));
 
-    
     // Filling server information
     servaddr.sin_family = AF_INET; // IPv4
     servaddr.sin_addr.s_addr = INADDR_ANY;
     servaddr.sin_port = htons(PORT);
 
     // Bind the socket with the server address
-    
+
     if (bind(sockfd, (const struct sockaddr *)&servaddr,
              sizeof(servaddr)) < 0)
     {
@@ -113,7 +113,6 @@ int main(int argc, char *argv[])
 
         printf("%ld\n", n);
 
-
         //print string message from client
         //printf("server received %ld/%ld bytes\n", sizeof(receivePacket), n);
         if (n > 0)
@@ -122,33 +121,74 @@ int main(int argc, char *argv[])
             printPacket(*receivePacket);
 
             //if the player doesn't exist then we append to the playerPacketArray
-            if (!checkIfPlayerPacketExists(&playerPacketArray, receivePacket->userName)){
+            if (!checkIfPlayerPacketExists(&playerPacketArray, receivePacket->userName))
+            {
                 addToArray(&playerPacketArray, *receivePacket);
+
+                packet confirmPack;
+                confirmPack.active = 1;
+                strcpy(confirmPack.userName, receivePacket->userName);
+                confirmPack.x = 0;
+                confirmPack.y = 0;
+
+                addPacketToBuffer(packetBuffer, confirmPack, &packetBufferIndex);
+
+                n = sendto(sockfd, (const char *)packetBuffer, sizeof(packetBuffer), MSG_CONFIRM, (const struct sockaddr *)&cliaddr, sizeof(cliaddr));
+
+                if (n > 0)
+                {
+                    printf("Sent confirmation message to client\n");
+                }
+
+                memset(packetBuffer, sizeof(packetBuffer), 1);
+                packetBufferSize = 0;
+                packetBufferIndex = 0;
+                memcpy(&(packetBuffer), &packetBufferSize, sizeof(packetBufferSize));
+
                 //packet replyLoginPacket = {3,0,0,"server"};
                 //sendto(sockfd, (const char *)test, sizeof(test), MSG_CONFIRM, (const struct sockaddr *)&cliaddr, len);
-
             }
             //if they do exist then we assume the game is running and this player
             //wants the information for all the other clients back
-            else{
+            else
+            {
                 updateClientInfo(&playerPacketArray, *receivePacket);
 
-                //player exists, update their information and check for no cheating
+                for (int i = 0; i < playerPacketArray.used; i++)
+                {
+                    
+                    if (strcmp(playerPacketArray.array[i].userName, receivePacket->userName) != 0)
+                    {
+                        addPacketToBuffer(packetBuffer, playerPacketArray.array[i], &packetBufferIndex);
+                    }
+                }
 
+                n = sendto(sockfd, (const char *)packetBuffer, sizeof(packetBuffer), MSG_CONFIRM, (const struct sockaddr *)&cliaddr, sizeof(cliaddr));
+
+                if (n > 0)
+                {
+                    printf("Sent update fetch message to client\n");
+                }
+
+                memset(packetBuffer, sizeof(packetBuffer), 1);
+                packetBufferSize = 0;
+                packetBufferIndex = 0;
+                memcpy(&(packetBuffer), &packetBufferSize, sizeof(packetBufferSize));
+
+                //player exists, update their information and check for no cheating
             }
-            
         }
 
         //print the playerarray if it is not empty
 
-        if (playerPacketArray.used > 0){
+        if (playerPacketArray.used > 0)
+        {
             printf("%s\n", playerPacketArray.array[0].userName);
         }
 
-        
         //send packetBuffer Array to client
-        int y = sendto(sockfd, (const char *)packetBuffer, sizeof(packetBuffer), MSG_CONFIRM, (const struct sockaddr *)&cliaddr, sizeof(cliaddr));
-        printf("Hello message sent %d.\n", y);
+        //int y = sendto(sockfd, (const char *)packetBuffer, sizeof(packetBuffer), MSG_CONFIRM, (const struct sockaddr *)&cliaddr, sizeof(cliaddr));
+        //printf("Hello message sent %d.\n", y);
     }
 
     return 0;
